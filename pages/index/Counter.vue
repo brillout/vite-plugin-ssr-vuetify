bbc5f26b85a649c03d854b22e38eb145062688df
Romuald Brillout <vite-plugin-ssr@brillout.com>
2021-07-16 18:28:04 +0200

    boilerplate Vite w/ vite-plugin-ssr

---
+++ pages/index/Counter.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-btn elevation="2" @click="state.count++">Counter {{ state.count }}</v-btn>
+</template>
+
+<script>
+import { reactive } from 'vue'
+export default {
+  setup(props) {
+    const state = reactive({ count: 0 })
+    return {
+      state
+    }
+  }
+}
+</script>
---
+++ pages/index/index.page.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Welcome</h1>
+  <p>This page is rendered to HTML and interactive.</p>
+  <br />
+  <p>
+    <Counter />
+  </p>
+</template>
+
+<script>
+import Counter from './Counter.vue'
+const components = { Counter }
+export default { components, data: () => ({}) }
+</script>
