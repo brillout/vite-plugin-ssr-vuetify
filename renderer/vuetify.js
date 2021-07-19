87db61bcd7bb351a1310e93bd3dab53731b7e49b
Romuald Brillout <git@brillout.com>
2021-07-19 17:54:31 +0200

    add .mjs extension

--- renderer/vuetify.js
+++ renderer/vuetify.js
@@ -1,8 +1,8 @@
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/lib/styles/main.sass'
 import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/lib/components'
-import * as directives from 'vuetify/lib/directives'
+import * as components from 'vuetify/lib/components/index.mjs'
+import * as directives from 'vuetify/lib/directives/index.mjs'
 
 export default createVuetify({
   components,
