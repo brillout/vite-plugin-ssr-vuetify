65548de486659c8b5ea331dc5c1314a52829b2d2
Romuald Brillout <git@brillout.com>
2021-09-06 11:31:55 +0200

    udpate dependencies

--- renderer/vuetify.js
+++ renderer/vuetify.js
@@ -1,8 +1,8 @@
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/lib/styles/main.sass'
 import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/lib/components/index.mjs'
-import * as directives from 'vuetify/lib/directives/index.mjs'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 export default createVuetify({
   components,
